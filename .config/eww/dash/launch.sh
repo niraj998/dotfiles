97a8a1153f3b35daa437645f5cdcc523f7377731
Niraj <nnk998@gmail.comm>
2022-02-18 00:53:04 +0530

    Updates

--- .config/eww/dash/launch.sh
+++ .config/eww/dash/launch.sh
@@ -1,15 +1,26 @@
 #bin/sh
 
 file=/tmp/ewwdash
+configdir=~/.config/eww/dash
+
+
+case $1 in
+  update)
+    eww -c $configdir reload
+    ;;
+    *)
 
 if [ ! -f "$file" ]; then
     touch $file
     sh ~/.config/eww/scripts/weather
-    eww -c ~/.config/eww/dash/ open-many bgblur profile monitors time system music directory power weather shortcuts search update speedmeter adblock
-#     eww -c ~/.config/eww/dash/ open-many profile monitors time system music directory power weather quote search update speedmeter adblock
+#    eww -c $configdir open-many bgblur profile monitors time system music directory power weather shortcuts search update speedmeter adblock
+     eww -c $configdir open-many profile monitors time system music directory power weather search update speedmeter adblock shortcuts
 else 
     rm $file
-   eww -c ~/.config/eww/dash/  close bgblur profile monitors time system music directory power weather shortcuts search update speedmeter adblock
-#     eww -c ~/.config/eww/dash/  close profile monitors time system music directory power weather quote search update speedmeter adblock
+#   eww -c $configdir close bgblur profile monitors time system music directory power weather shortcuts search update speedmeter adblock
+#      eww -c $configdir close profile monitors time system music directory power weather search update speedmeter adblock shortcuts
+    eww -c $configdir kill
 
 fi
+  ;;
+esac
